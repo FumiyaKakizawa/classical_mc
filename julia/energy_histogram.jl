10d630a296bb8250228057a08f277207e35b7db0
FumiyaKakizawa <f.kakizawa.178@ms.saitama-u.ac.jp>
2021-06-18 11:34:11 +0900

    minor correction.

--- julia/energy_histogram.jl
+++ julia/energy_histogram.jl
@@ -2,6 +2,8 @@ using HDF5
 using PyPlot
 
 h5file_name = "2d_out.h5"
+J2 = -0.01
+L = 36
 
 function load_from_h5file(h5file_name)
     
@@ -13,14 +15,17 @@ end
 
 ene_hist,num_temps = load_from_h5file(h5file_name)
 
-function mk_hist(ene_hist,num_temps)
+function mk_hist(ene_hist,num_temps,J2_val,L_val)
     
     for it in 1:num_temps
         plt.figure()
+        plt.xlabel("Energy")
+        plt.ylabel("count of appearance")
+        plt.title("Energy histogram at J2=$(J2_val),L=$(L_val)")
         plt.hist(ene_hist[:,it])
         plt.savefig("ene_hist_$(it)")
         plt.close()
     end
 end
 
-mk_hist(ene_hist,num_temps)
+mk_hist(ene_hist,num_temps,J2,L)
